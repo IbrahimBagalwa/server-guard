78a90a97b49a2de3a21c2756e42e31bc8c8f3723
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2026-04-23 17:47:52 +0200

    feat: enhance monitoring by adding memory and disk usage tracking

--- lib/monitor.sh
+++ lib/monitor.sh
@@ -8,7 +8,30 @@ get_cpu_usage(){
     	cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
     else
 	    echo "Unsupported OS"
-	return 1
+      return 1
+      fi
+    echo $cpu
+}
+
+get_memory_usage(){
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      used=$(vm_stat | grep "Pages active" | awk '{print $3}' | tr -d '.')
+      free=$(vm_stat | grep "Pages free" | awk '{print $3}' | tr -d '.')
+      total=$((used + free))
+
+      usage=$(echo "($used / $total) * 100" | bc -l)
+
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      usage=$(free | grep Mem | awk '{print ($3/$2) * 100}')
+    else
+      echo "Unsupported OS"
+      return 1
     fi
-   echo $cpu
+
+    echo $usage
+}
+
+get_disk_usage(){
+    usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
+    echo $usage
 }
--- server_guard.sh
+++ server_guard.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
-source lib/*
+source lib/monitor.sh
+source lib/logger.sh
 
 COMMAND=$1
-
 case $COMMAND in
     monitor)
-	CPU_USAGE=$(get_cpu_usage)
+		CPU_USAGE=$(get_cpu_usage)
+		MEM_USAGE=$(get_memory_usage)
+		DISK_USAGE=$(get_disk_usage)
+	
         log "INFO" "CPU Usage: $CPU_USAGE%"
+        log "INFO" "Memory Usage: $MEM_USAGE%"
+        log "INFO" "Disk Usage: $DISK_USAGE%"
 	;;
     fix)
 		log "INFO" "Recovery started"
